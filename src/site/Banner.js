761f38f3d1f607827ea93151901d7685c4cffb9b
v4nd3r <vandergagno@gmail.com>
2020-05-04 23:08:28 -0300

    Update Banner.js

--- src/site/Banner.js
+++ src/site/Banner.js
@@ -7,7 +7,7 @@ function Banner(props) {
             <div className="container">
                 <div className="row no-gutters slider-text js-fullheight align-items-center justify-content-center" data-scrollax-parent="true">
                 <div className="col-md-10 ftco-animate text-center" data-scrollax=" properties: { translateY: '70%' }">
-                    <h1 className="mb-4" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Igreja Videira Vitória</h1>
+                    <h1 className="mb-4" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Igreja Videira Vitória teste</h1>
                     <p data-scrollax="properties: { translateY: '30%', opacity: 1.6 }"><a href="#divQuemSomos" className="btn btn-primary btn-outline-white px-4 py-3">Saiba Mais</a></p>
                 </div>
             </div>
